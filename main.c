5715206955fcf40b13d3a13331d8bc87891f09c3
souhaib101 <souhaib.alabdeh@gmail.com>
2022-09-04 15:51:12 +0200

    definig  struct and enum

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+
+int main(int argc, char** argv){
+    
+    /*
+    at least we got 3 string input {"./puzzle", "eingabe.txt", "ausgabe.txt"}
+    */
+    if(argc <= 3){
+        return 1;
+    }
+
+
+    /*TODO:
+    read all input datei and store it in array from type puzzle, each puzzle should save one file.
+    the file is formatted
+    */
+
+    
+
+   
+   return 0;
+}
